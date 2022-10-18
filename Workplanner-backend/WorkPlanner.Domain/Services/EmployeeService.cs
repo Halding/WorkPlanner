6679af86a77833721c63f7714ffd7efc1ae68c21
niko4730 <niko4730@easv365.dk>
2022-10-18 11:48:31 +0200

    Update EmployeeService.cs

--- Workplanner-backend/WorkPlanner.Domain/Services/EmployeeService.cs
+++ Workplanner-backend/WorkPlanner.Domain/Services/EmployeeService.cs
@@ -14,6 +14,7 @@ public class EmployeeService : IEmployeeService
         _employeeRepository = employeeRepository;
     }
 
+    //Gets all employees in the list
     public List<Employee> GetAllEmployees()
     {
         return _employeeRepository.ReadAll();
