d6ec72bbbacc6a477e7669d22cda76c3ac300a76
Svend Halding <sh@onlimited.dk>
2022-11-08 13:29:41 +0100

    still in progress123

--- workplanner-frontend/components/CalendarUi.tsx
+++ workplanner-frontend/components/CalendarUi.tsx
@@ -6,22 +6,24 @@ import {startOfWeek, format, parse, getDay} from "date-fns";
 import Layout from "./Layout";
 import {Employee} from "../models/Employee";
 import {getCookie} from "./Login";
+import {Shift} from "../models/Shift";
 
 
-export const getStaticProps = async () => {
-    let jwt = getCookie("OurJwt")
-    const res = await fetch(`http://localhost:5293/api/employee/user`, {
-        headers: {
-            Authorization: "Bearer " + jwt
-        }
-    });
-    const data = await res.json();
-    console.log(data)
-
-    return {
-        props: {user: data}
-    }
-}
+// export const getStaticProps = async () => {
+//     let jwt = getCookie("OurJwt")
+//
+//     const res = await fetch(`https://localhost:7293/api/shift/test`, {
+//         headers: {
+//             Authorization: "Bearer " + jwt
+//         }
+//     });
+//     const data = await res.json();
+//     console.log(data)
+//
+//     return {
+//         props: {user: data}
+//     }
+// }
 
 const locales = {
     "da": require("date-fns/locale/da")
@@ -67,23 +69,27 @@ function CalendarUi() {
     const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""})
     const [allEvents, setAllEvents] = useState(events)
     const [employee, setEmployee] = useState<Employee>()
-
+    const [allShifts, setAllShifts] = useState<Shift[]>()
 
 
 
     const getUser = async () => {
 
         let jwt = getCookie("OurJwt")
-        const {data: userGotById} = await axios.get(`http://localhost:5293/api/employee/user`, {
+        const {data: usersShifts} = await axios.get(`https://localhost:7293/api/shift/test`, {
             headers: {
                 Authorization: "Bearer " + jwt
             }
         }).
         then((res) => res.data);
-        setEmployee(userGotById)
+        console.log("yo")
+        console.log(usersShifts)
+        console.log("yo")
+
+        setAllShifts(usersShifts)
 
         console.log("her")
-        console.log("")
+        console.log(allShifts)
         console.log("her")
     };
 
---
+++ workplanner-frontend/models/Shift.tsx
@@ -0,0 +1,9 @@
+export type Shift = {
+    id: number,
+    startTime: string,
+    endTime: string,
+    clockInTime: null
+    clockOutTime: null,
+    employeeId: number,
+    departmentId: number
+};
